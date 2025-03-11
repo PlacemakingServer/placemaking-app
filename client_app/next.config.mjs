faeb518409220f9d62ef0ee118ebfaef9a475089
Eduardo Marcelino <eduardoofficial12@gmail.com>
2025-03-11 10:04:34 -0300

    updates

--- client_app/next.config.mjs
+++ client_app/next.config.mjs
@@ -9,5 +9,20 @@ export default withPWA({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
   register: true,  
+  register: true,
   skipWaiting: true,
+  runtimeCaching: [
+    {
+      urlPattern: /^https?.*/,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'pages-cache',
+        networkTimeoutSeconds: 15,
+        expiration: {
+          maxEntries: 50,
+          maxAgeSeconds: 7 * 24 * 60 * 60,
+        },
+      },
+    },
+  ],
 })(nextConfig);
