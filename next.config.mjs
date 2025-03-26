5f3f71853a1c6201903ffa3a7db6d2fc27f3a3ff
Eduardo Marcelino <eduardoofficial12@gmail.com>
2025-03-26 18:50:48 -0300

    fix: add jsconfig for @ alias 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+const ___dirname = dirname(fileURLToPath(import.meta.url))
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack(config) {
-    config.resolve.alias['@'] = `${__dirname}/src`
+    config.resolve.alias['@'] = `${___dirname}/src`
     return config
   }
 }
