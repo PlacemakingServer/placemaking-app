dfcd350bcacf60e046b2523116c05ad0b357ab32
Eduardo Marcelino <eduardoofficial12@gmail.com>
2025-04-03 22:05:12 -0300

    rotas de research

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,3 @@
-// Exemplo em _document.js
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
@@ -6,6 +5,10 @@ class MyDocument extends Document {
     return (
       <Html lang="pt-BR">
         <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+          />
           <link rel="manifest" href="/manifest.json" />
           <meta name="theme-color" content="#000000" />
           <link rel="apple-touch-icon" href="/img/521-azul.png" />
