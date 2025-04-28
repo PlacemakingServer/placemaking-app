b20d79aaa7655bd12f4219c077e3b0178a141ce9
Eduardo Marcelino <eduardoofficial12@gmail.com>
2025-04-28 19:56:00 -0300

    merge dashboard

--- src/pages/researches/teste/surveys/[surveyid]/answers.js
+++ src/pages/researches/teste/surveys/[surveyid]/answers.js
@@ -7,7 +7,7 @@ import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
 import { Label } from '@/components/ui/label';
-import { Button } from '@/components/ui/button';
+import  Button  from '@/components/ui/Button';
 import { useMessage } from "@/context/MessageContext";
 import SurveyLoader from '@/components/surveys/SurveyLoader';
 
